061535da30840bd417448e903742aff1c92e09ab
MSTELLA <m_stella@live.concordia.ca>
2022-01-25 15:45:03 -0500

    Testing Commits

---
+++ Orders/Orders.cpp
@@ -0,0 +1,16 @@
+#includes Orders.h;
+
+class OrderList {
+	// Has a vector of Orders
+	boolean remove(int i);
+	boolean move(int i, int j);
+};
+
+class Order {
+	// has a type
+	boolean validate();
+	//Implementation next assignment
+
+	boolean execute();
+	//Implementation next assignment
+};
---
+++ Orders/Orders.h
@@ -0,0 +1,19 @@
+// Orders
+// class attributes: Order Type , probably a method for each type to be implemented next assignment
+// stream insertion operator that output strings description of order and effect of the order if correctly executed
+
+
+
+class OrderList {
+
+	boolean remove(int i);
+	boolean move(int i, int j);
+};
+
+class Order {
+	boolean validate();
+	//Implementation next assignment
+
+	boolean execute();
+	//Implementation next assignment
+};
