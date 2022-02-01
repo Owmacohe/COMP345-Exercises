1e9078c4355f38ce882853851dd700d6495c51ea
Owmacohe <44061383+Owmacohe@users.noreply.github.com>
2022-02-01 12:39:24 -0500

    Started changing from vector to pointer array

--- Map/MapDriver.cpp
+++ Map/MapDriver.cpp
@@ -1,7 +1,5 @@
-#include <iostream>
 #include <fstream>
 #include <sstream>
-#include <vector>
 using namespace std;
 
 #include "Map.cpp"
@@ -9,14 +7,24 @@ using namespace std;
 int mapMain() {
     cout << endl;
 
-    MapLoader loader = MapLoader();
-    Map europe = loader.load("europe.map");
-    Map canada = loader.load("canada.map");
-    cout << europe << endl;
+    Map temp = Map("MyMap");
+    temp.addContinent("MyContinent");
+    cout << temp << endl;
+
+    //MapLoader loader = MapLoader();
+    //Map europe = loader.load("europe.map");
+    //Map canada = loader.load("canada.map");
+    //cout << europe << endl;
     //cout << canada << endl;
 
     // TODO create a Map for each supplied .map file using Maploader
     // TODO use validate() for each Map
+
+    return 0;
+}
+
+int main() {
+    mapMain();
 
     return 0;
 }
