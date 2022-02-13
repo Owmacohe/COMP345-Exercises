7f4b32e89135c847884e56f180000131d1bfc0b4
Owmacohe <44061383+Owmacohe@users.noreply.github.com>
2022-02-13 13:31:48 -0500

    Map is DONE!

--- Cards/Cards.cpp
+++ Cards/Cards.cpp
@@ -155,12 +155,12 @@ Deck& Deck::operator=(const Deck& toAssign){
 /*********************************** HAND ***********************************/
 
 Hand::Hand(){
-    cout << "Hand Default Constructor" << endl;
     numCardInHand = 0;
     hand = vector<Card*>();
+
+    //cout << "Hand Default Constructor" << endl;
 }
 Hand::Hand(Hand &anotherHand){
-    cout << "Hand Copy Constructor" << endl;
     numCardInHand = anotherHand.numCardInHand;
 
     hand = vector<Card*>();
@@ -168,6 +168,8 @@ Hand::Hand(Hand &anotherHand){
         Card* cards = new Card(*anotherHand.hand.at(i));
         hand.push_back(cards);
     }
+
+    //cout << "Hand Copy Constructor" << endl;
 }
 
 Hand::~Hand(){
@@ -176,9 +178,10 @@ Hand::~Hand(){
         hand.at(i) = NULL;
         cout << "...delete Cards on Hand" << endl;
     }
-    cout << "Hand deleted" << endl;
     // delete the Hand vector
     hand.clear();
+
+    //cout << "Hand deleted" << endl;
 }
 // Overload <<
 ostream& operator<<(ostream& os, const Hand& h)
