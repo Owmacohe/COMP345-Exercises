fe08216b02f5383bf37e08c5ed1e09b2dc8c3c3f
MSTELLA <m_stella@live.concordia.ca>
2022-03-01 20:03:46 -0500

    LogObs h & cpp. Added to Order only

--- LoggingObserver/LoggingObserver.h
+++ LoggingObserver/LoggingObserver.h
@@ -1,34 +1,44 @@
 #pragma once
 #include <iostream>
+#include <fstream>
+#include <list>
 
-class Subject{
-    void attach(Observer* o);
-    void detach(Observer* o);
-    void notify();
+using namespace std;
+
+class Iloggable{
+public:
+    string virtual stringToLog()=0;
 };
 
 class Observer{
-
-    ~Observer();
-    virtual void update() = 0;
-
-protected:
+public:
     Observer();
+    ~Observer();
+    virtual void update(Iloggable* il) = 0;
 };
 
-class Iloggable{
-    string virtual stringTolog();
+class LogObserver : public Observer{
+public:
+    string fileName;
+    ofstream outputFile;
+
+    LogObserver();
+    LogObserver(string fileName);
+    ~LogObserver();
+    void update(Iloggable* il);
 };
 
-class LogObserver{};
+class Subject {
+public:
+    Subject();
+    ~Subject();
 
-/**
- * Subjects of logging mechanism that will have to inherit Iloggable:
- * Command
- * CommandProcessor
- * FileCommandProcessorAdapter
- * OrderList
- * Order
- * GameEngine
- *
-*/
+    void attach(Observer *obs);
+    void detach(Observer *obs);
+    void notify(Iloggable* il);
+
+    public:
+    list<Observer*> observers;
+    // Does not use collection? Nope...will pass(this) in update method to do a many to one rather than one to many relationship between Subject and Observer
+    // Will still implement it just ot have it and I will ask TA if necessary
+};
--- LoggingObserver/LoggingObserverDriver.cpp
+++ LoggingObserver/LoggingObserverDriver.cpp
@@ -1,4 +1,24 @@
-//
-// Created by Caste on 2022-03-01.
-//
+#include "LoggingObserver.h"
+#include "../Cards/Cards.h"
+#include "../Orders/Orders.h"
+#include "../Map/Map.h"
+#include "../Player/Player.h"
+#include "../GameEngine/GameEngine.h"
 
+#include "../Cards/Cards.cpp"
+#include "../Orders/Orders.cpp"
+#include "../Map/Map.cpp"
+#include "../Player/Player.cpp"
+#include "../GameEngine/GameEngine.cpp"
+
+int LoggingObserverMain() {
+
+//            (1) Show that classes inherit from subject and Iloggable
+//            (2) SaveCommand, Execute, saveEffect, addOrder, Transition methods use Notify(Subject)
+//            (3) gamelog.txt file is correctly written
+//            (4) addOrder to Orderlist from a player actually outputs on file
+//            (5) When an Order is executed, output to file
+//            (6) GameEngine changes state, output to file
+
+    return 0;
+}
