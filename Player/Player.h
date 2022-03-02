55ae8bb1cf864abf3ec71a391c8b4c1582a16f1c
guidote <74155514+guidote@users.noreply.github.com>
2022-03-02 01:38:55 +0000

    created an army class to keep track of where an army is because this is needed for player to determine where to move a players armies
    as u can see in the code, player.h and player.cpp includes the army class. i still need to update the player.cpp player section to update constructors and such to include army. these army  related variables (num_armies, num_reinforcements, armies) will be needed for the reinforcement and issue order sections of part 3.

--- Player/Player.h
+++ Player/Player.h
@@ -36,6 +36,9 @@ class Player {
         Player& operator = (const Player& player); 	// Assignment Operator
         friend ostream& operator<<(ostream& os, const Player& player); // Stream insertion operator
 
+        void addArmy(const Army &a); // Method to add army to array of pointers
+        void removeArmy(const Army &a); // Method to remove army from array of pointers
+
     private:
         string name; // Name of Player
         vector<Territory*> territories; // List of players territories
@@ -43,7 +46,34 @@ class Player {
         OrdersList* orders; // Player list of orders
 
         //added with a2
-        int num_armies;
-        //probably have to created a class reinforcement pool to hold armies, and have each player have a reinforcement pool
-        //check until reinforcement pool is empty then all armies are either attacking or defending
+        int num_armies; // Total number of armies player has
+        int num_reinforcement; // Number of reinforment armies player has
+
+        Army *armies; // Array of army pointers that are defending or attacking
+};
+
+//added with a2
+class Army {
+    public:
+
+        Army(); // Default constructor
+        Army(int, Territory* = NULL); // Parameterized constructor
+        Army(const Army &a); // Copy constructor
+
+        ~Army(); // Destructor
+
+        // Mutators
+        int getA_Num();
+        Territory* getLocated();
+
+        // Accessors
+        void setA_Num(int n), setLocated(Territory* t);
+
+        
+        Army& operator = (const Army& Army); 	// Assignment Operator
+        friend ostream& operator<<(ostream& os, const Army& Army); // Stream insertion operator
+
+    private:
+        Territory* located; // Where army is located
+        int a_num; // Number of army
 };
