5606f01a7e2800a22a65d87d2092d826f476bd9a
purpowl <ldbaongan.2701@gmail.com>
2022-03-07 14:51:03 -0500

    Deploy, Advance, Airlift constructor, Player reinforcementpool

--- Player/Player.h
+++ Player/Player.h
@@ -10,6 +10,7 @@ using namespace std;
 class Hand;
 class OrdersList;
 class Territory;
+class Map;
 
 class Player {
     public:
@@ -19,21 +20,23 @@ class Player {
 
         ~Player(); // Destructor
 
-//        vector<Territory*> toDefend(Map); // Territories for player to defend
-//        vector<Territory*> toAttack(Map); // Territories for player to attack
+        vector<Territory*> toDefend(Map m); // Territories for player to defend
+        vector<Territory*> toAttack(Map m); // Territories for player to attack
 
         void issueOrder(string); //  Create order and adds order to players order list
 
         int getNumberOfArmies(); // Get number of armies player owns
-
+        void addToReinforcePool(int armies);
+        void removeFromReinforcePool(int armies);
         // Accessors
-        void setName(string n), setTerritory(vector<Territory*> t), setHand(Hand* h), setOrder(OrdersList* o);
+        void setName(string n), setTerritory(vector<Territory*> t), setHand(Hand* h), setOrder(OrdersList* o), setReinforcementPool(int armies);
 
         // Mutators
         string getName();
         vector<Territory*> getTerritory();
         Hand* getHand();
         OrdersList* getOrder();
+        int getReinforcePool();
 
         Player& operator = (const Player& player); 	// Assignment Operator
         friend ostream& operator<<(ostream& os, const Player& player); // Stream insertion operator
@@ -43,4 +46,5 @@ class Player {
         vector<Territory*> territories; // List of players territories
         Hand* hand; // Player hand
         OrdersList* orders; // Player list of orders
+        int reinforcePool;
 };
