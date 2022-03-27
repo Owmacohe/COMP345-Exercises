ba6c323445b024c16491b001df16706919c1d36f
MSTELLA <m_stella@live.concordia.ca>
2022-03-27 01:48:37 -0400

    LogObgs Driver

--- Orders/Orders.cpp
+++ Orders/Orders.cpp
@@ -164,7 +164,7 @@ void Deploy::execute() {
     } else {
         cout << "Execution fail!\n" << endl;
     }
-//   notify(this);
+   notify(this);
 }
 
 string Deploy::stringToLog() {
@@ -368,7 +368,7 @@ void Advance::execute() {
     } else
         cout << "Can't execute Advance order!" << endl;
 
-//    notify(this);
+    notify(this);
 }
 
 int Advance::deathCalculation(int qty, double probability) {
@@ -535,7 +535,7 @@ void Airlift::execute() {
     } else{
         cout << "Execution failed!\n" << endl;
     }
-//     notify(this);
+     notify(this);
 }
 
 
@@ -663,7 +663,7 @@ void Bomb::execute() {
         target->setArmies(bombedArmies);
         cout << "Execution successful! \n" << endl;
     }
-//    notify(this);
+    notify(this);
 }
 
 string Bomb::stringToLog() {
@@ -882,7 +882,7 @@ void Negotiate::execute() {
         game->addAlliances(playerIssuing, targetPlayer);
         cout << "Execution successful!\n" << endl;
     }
-//    notify(this);
+    notify(this);
 }
 
 string Negotiate::stringToLog() {
@@ -1010,7 +1010,7 @@ ostream &operator<<(ostream &os, const OrdersList &ordersList) {
 
 void OrdersList::addOrder(Order *order) {
     playerOrderList.push_back(order);
-//    notify(this);
+    notify(this);
 }
 
 // Remove Method used to remove an order of the OrderList. The Parameter is an int for the index of the Order.
