b2a734a23bda6137bb8e57d9fd921ae8338221b1
guidote <74155514+guidote@users.noreply.github.com>
2022-04-06 16:05:13 -0400

    human player done audrey i can explain my reasoning to u and we can change whatever is necessary

--- Player/Player.h
+++ Player/Player.h
@@ -16,54 +16,58 @@ class Map;
 class PlayerStrategies;
 
 class Player {
-public:
-    Player(); // Default constructor
-    Player(string, vector<Territory*>, Hand*, OrdersList*, int); // Parameterized constructor (everything)
-    Player(string); // Parameterized constructor (name only)
-    Player(const Player &p); // Copy constructor
+    public:
+        Player(); // Default constructor
+        Player(string, vector<Territory*>, Hand*, OrdersList*, int); // Parameterized constructor (everything)
+        Player(string); // Parameterized constructor (name only)
+        Player(const Player &p); // Copy constructor
 
-    ~Player(); // Destructor
+        ~Player(); // Destructor
 
-    vector<Territory*> toDefend(Map* m); // Territories for player to defend
-    vector<Territory*> toAttack(Map* m); // Territories for player to attack
+        vector<Territory*> toDefend(Map* m); // Territories for player to defend
+        vector<Territory*> toAttack(Map* m); // Territories for player to attack
 
-    void issueOrder(string type); //  Create order and adds order to players order list
+        void issueOrder(string type); //  Create order and adds order to players order list
 
-    int getNumberOfArmies(); // Get number of armies player has
-    int getNumberOfTerritories(); // Get number of territories player owns
-    void addToReinforcePool(int armies); // Add to reinforcement pool
-    void removeFromReinforcePool(int armies); // Remove from reinforcement pool
+        int getNumberOfArmies(); // Get number of armies player has
+        int getNumberOfTerritories(); // Get number of territories player owns
+        void addToReinforcePool(int armies); // Add to reinforcement pool
+        void removeFromReinforcePool(int armies); // Remove from reinforcement pool
+        Territory*  checkTerritoryOwn(string name); // Check if player owns a territory by name
 
-    Territory* getOriginTerritory(Territory* target_territory, Map* m); // Returns a territory of the player that is adjacent to the one they would like to attack
+        Territory* getOriginTerritory(Territory* target_territory, Map* m); // Returns a territory of the player that is adjacent to the one they would like to attack
 
-    // Accessors
-    void setName(string n), setTerritory(vector<Territory*> t), setHand(Hand* h), setOrder(OrdersList* o), setReinforcementPool(int armies);
+        // Accessors
+        void setName(string n), setTerritory(vector<Territory*> t), setHand(Hand* h), setOrder(OrdersList* o), setReinforcementPool(int armies), setStrategy(PlayerStrategies* ps);
 
-    // Mutators
-    void assignTerritory(Territory* t);
-    void removeTerritory(int index);
-    string getName();
-    vector<Territory*> getTerritoryList();
-    Hand* getHand();
-    OrdersList* getOrder();
-    int getReinforcePool();
-    OrdersList* getDeployList();
+        // Mutators
+        void assignTerritory(Territory* t);
+        void removeTerritory(int index);
+        void addOrderList(Order* o);
+        string getName();
+        vector<Territory*> getTerritoryList();
+        Hand* getHand();
+        OrdersList* getOrder();
+        int getReinforcePool();
+        OrdersList* getDeployList();
+        PlayerStrategies* getPlayerStrategy();
 
-    Player& operator = (const Player& player); 	// Assignment Operator
-    friend ostream& operator<<(ostream& os, const Player& player); // Stream insertion operator
+        Player& operator = (const Player& player); 	// Assignment Operator
+        friend ostream& operator<<(ostream& os, const Player& player); // Stream insertion operator
 
-    // Assignment 3
-    vector<Territory*> toDefend(Map* m, PlayerStrategies* ps); // Territories for player to defend
-    vector<Territory*> toAttack(Map* m, PlayerStrategies* ps); // Territories for player to attack
-    void issueOrder(PlayerStrategies* ps); //  Create order and adds order to players order list
-private:
-    string name; // Name of Player
-    vector<Territory*> territories; // List of players territories
-    Hand* hand; // Player hand
-    OrdersList* orders; // Player list of orders
-    int reinforcePool; // Number of reinforcements player has
-    OrdersList* deployList; // List of deploy orders to be executed before the other orders
+        // Assignment 3
+        vector<Territory*> toDefend(Map* m, PlayerStrategies* ps); // Territories for player to defend
+        vector<Territory*> toAttack(Map* m, PlayerStrategies* ps); // Territories for player to attack
+        void issueOrder(string input, PlayerStrategies* ps); //  Create order and adds order to players order list
 
-    // Strategy pattern implementation
-    PlayerStrategies* playerStrategy;
+    private:
+        string name; // Name of Player
+        vector<Territory*> territories; // List of players territories
+        Hand* hand; // Player hand
+        OrdersList* orders; // Player list of orders
+        int reinforcePool; // Number of reinforcements player has
+        OrdersList* deployList; // List of deploy orders to be executed before the other orders
+
+        // Strategy pattern implementation
+        PlayerStrategies* playerStrategy;
 };
--- PlayerStrategy/PlayerStrategies.cpp
+++ PlayerStrategy/PlayerStrategies.cpp
@@ -1,22 +1,98 @@
 #include "PlayerStrategies.h"
-#include <iostream>
-#include <vector>
+#include "../Orders/Orders.h"
+#include "../Player/Player.h"
+#include "../Map/Map.h"
 
-using namespace std;
 /****************************** Human Player Strategy*******************************/
-/* requires user interactions to make decisions
+/*
+ * Requires user interaction :
+ * User chooses whether advance is to move or to attack
+ * User chooses which territory is returned from their toAttack() method
+ * User chooses which territory is returned from their toDefend() method
  */
 
-void HumanPlayerStrategy::issueOrder() {
+void HumanPlayerStrategy::issueOrder(string type) {
+    // Set to all lowercase
+    transform(type.begin(), type.end(), type.begin(), ::tolower);
+    string input;
 
+    if (type == "deploy") {
+        // Issues deploy orders based on toDefend()
+        Deploy* d = new Deploy(p);
+        p->addOrderList(d);// Add order to list
+    }
+    else if (type == "advance") {
+        cout << "\nWhat type of advance would " << p->getName() << " Player like to issue ? (move, attack, none)" << endl;
+        cin >> input;
+        while (!equalsIgnoreCase("move", input) || !equalsIgnoreCase("attack", input) || !equalsIgnoreCase("none", input)) {
+            cout << "\nInvalid input ! Try Again." << endl;
+            cin >> input;
+        }
+        if (!equalsIgnoreCase("none", input)) {
+            Advance* a = new Advance(p, input);
+            p->addOrderList(a);
+        }
+    }
+        // Orders that involve card will be issued using playCard()
+    else {
+        cout << "Invalid order" << endl;
+    }
 }
 
-vector<Territory*> HumanPlayerStrategy::toAttack() {
+vector<Territory*> HumanPlayerStrategy::toAttack(Map* m) {
+    vector<Territory*> returnTerritories;
+    string input;
 
+    cout << "\nWhat territory would" << p->getName() << " Player like to attack from ?" << endl;
+    cin >> input;
+
+    while (p->checkTerritoryOwn(input) == NULL) {
+        cout << "\nInvalid input " << p->getName() << "Player does not own territory ! Try Again." << endl;
+        cin >> input;
+    }
+
+    Territory* territory = p->checkTerritoryOwn(input);
+    vector<Territory*> territories = m->getConnectedTerritories(territory->getName());
+
+    cout << "\nChosen territory : " << input <<". What territory would" << p->getName() << " Player like to attack ?" << endl;
+    cin >> input;
+    bool flag = false;
+    while (flag == false) {
+        for (Territory* t : territories) {
+            if (equalsIgnoreCase(t->getName(), input)) {
+                returnTerritories.push_back(t);
+                flag = true;
+                break;
+            }
+        }
+        if (flag == false) {
+            cout << "\nInvalid input " << p->getName() << "Player does not own territory ! Try Again." << endl;
+            cin >> input;
+        }
+    }
+    return returnTerritories;
 }
 
-vector<Territory*> HumanPlayerStrategy::toDefend() {
+vector<Territory*> HumanPlayerStrategy::toDefend(Map* m) {
+    vector<Territory*> returnTerritories;
+    string input;
 
+    cout << "\nWhat territory would" << p->getName() << " Player like to defend ?" << endl;
+    cin >> input;
+
+    while (p->checkTerritoryOwn(input) == NULL) {
+        cout << "\nInvalid input " << p->getName() << "Player does not own territory ! Try Again." << endl;
+        cin >> input;
+    }
+
+    vector<Territory*> territories = p->getTerritoryList();
+    for (Territory* t : territories) {
+        if (equalsIgnoreCase(t->getName(), input)) {
+            returnTerritories.push_back(t);
+            break;
+        }
+    }
+    return returnTerritories;
 }
 
 /****************************** Aggressive Player Strategy*******************************/
@@ -25,7 +101,7 @@ vector<Territory*> HumanPlayerStrategy::toDefend() {
  * always advances to enemy territories until it cannot do so anymore
  */
 
-void AggressivePlayerStrategy::issueOrder() {
+void AggressivePlayerStrategy::issueOrder(string type) {
 
 }
 
@@ -34,7 +110,7 @@ vector<Territory*> AggressivePlayerStrategy::toAttack() {
 }
 
 vector<Territory*> AggressivePlayerStrategy::toDefend() {
-
+    // Aggressive player has no use for toDefend()
 }
 
 /****************************** Benevolent Player Strategy*******************************/
@@ -56,19 +132,20 @@ vector<Territory*> BenevolentPlayerStrategy::toDefend() {
 }
 
 /****************************** Neutral Player Strategy*******************************/
-/* computer player that never issues any order
+/*
+ * Player never issues any orders
  * If a Neutral player is attacked, it becomes an Aggressive player
  */
-void NeutralPlayerStrategy::issueOrder() {
-
+void NeutralPlayerStrategy::issueOrder(string type) {
+    // Neutral player never issues orders, and therefore issueOrder() remains empty
 }
 
-vector<Territory*> NeutralPlayerStrategy::toAttack() {
-
+vector<Territory*> NeutralPlayerStrategy::toAttack(Map* m) {
+    // Neutral player doesn't issue orders, and therefore has no use for a toAttack()
 }
 
-vector<Territory*> NeutralPlayerStrategy::toDefend() {
-
+vector<Territory*> NeutralPlayerStrategy::toDefend(Map* m) {
+    // Neutral player doesn't issue orders, and therefore has no use for a toDefend()
 }
 
 /****************************** Cheater Player Strategy*******************************/
@@ -85,4 +162,18 @@ vector<Territory*> CheaterPlayerStrategy::toAttack() {
 
 vector<Territory*> CheaterPlayerStrategy::toDefend() {
 
+}
+
+// Checks for returns true for strings that are equal ignoring case
+bool equalsIgnoreCase(string s1, string s2) {
+    // Change to lower case
+    transform(s1.begin(), s1.end(), s1.begin(), ::tolower);
+    transform(s2.begin(), s2.end(), s2.begin(), ::tolower);
+
+    if (s1 == s2) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
