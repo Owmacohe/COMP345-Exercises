79d572b34ddb90c7145cfae6d3444e3d68a725a2
Youssef Morcos <youssef_samir@live.com>
2022-04-16 03:22:52 -0400

    bug fixes and testing done

--- PlayerStrategy/PlayerStrategies.cpp
+++ PlayerStrategy/PlayerStrategies.cpp
@@ -340,19 +340,20 @@ void BenevolentPlayerStrategy::issueOrder(string type) {
             if (p->getHand()->hand[i]->getType() == "airlift"){
                 indices.push_back(i);
             }
-            if (p->getHand()->hand[i]->getType() == "negotiate"){
-                indices.push_back(i);
-            }
             if (p->getHand()->hand[i]->getType() == "blockade"){
                 indices.push_back(i);
             }
         }
-        //shuffle the vector
-         shuffle(indices.begin(), indices.end(),default_random_engine());
+        if(indices.size()>0) {
+            //shuffle the vector
+            shuffle(indices.begin(), indices.end(), default_random_engine());
 
-        //pick the first card in the vector to be played
-        p->getHand()->playCard(indices[0], *game->getDeck(), *p->getOrder(), p);
-    }
+            //pick the first card in the vector to be played
+            p->getHand()->playCard(indices[0], *game->getDeck(), *p->getOrder(), p);
+        }else{
+            cout<<"There is no airlift or blockade to be played, only other types"<<endl;
+        }
+        }
     else {
         cout << "Invalid order" << endl;
     }
@@ -457,7 +458,7 @@ void CheaterPlayerStrategy::issueOrder(string type) {
 
     if (equalsIgnoreCase("deploy", type)) {
         //Deploy in a random territory on his list
-        Deploy* d = new Deploy(p,p->getTerritoryList().at((rand()*p->getNumberOfTerritories())+1));
+        Deploy* d = new Deploy(p);
         p->addOrderList(d); // Add order to the list
     }
     //TODO : we shouldn't use advance tho because its an automatic ownership of all surrounding territories
@@ -474,13 +475,14 @@ void CheaterPlayerStrategy::issueOrder(string type) {
             territoriesToBeStolen = m->getConnectedTerritories(t->getName());
             for(Territory * ter: territoriesToBeStolen){
                 ter->setOwner(p);
+                p->assignTerritory(ter);
                 if (ter->getOwner()->getPlayerStrategy()->getType() == "Neutral") {
                     setNeutralAttack(ter->getOwner());
                 }
             }
         }
     }
-    else if ((equalsIgnoreCase("card", type))) ; // Do nothing
+    else if ((equalsIgnoreCase("card", type))){cout<<"no cards are allowed for this strategy"<<endl;}  // Do nothing
     else {
         cout << "Invalid order" << endl;
     }
@@ -495,9 +497,10 @@ vector<Territory*> CheaterPlayerStrategy::toAttack() {
 }
 
 vector<Territory*> CheaterPlayerStrategy::toDefend() {
-    cout << "toDefend() in Cheater Strategy called, ERROR" << endl;
+    cout << "toDefend() in Cheater Strategy called" << endl;
     vector<Territory*> defend_territories = vector<Territory*>();
     Map* m = game->getMap();
+    defend_territories = p->getTerritoryList();
     return defend_territories;
 }
 
